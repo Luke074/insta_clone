f0e53d5a83fe41c40db37850c11296240e473117
Luke074 <mendeslucas277@gmail.com>
2021-07-07 22:44:58 -0300

    first commit

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  host: "localhost",
+  username: "root",
+  password: "bcd127",
+  database: "clone_insta",
+  dialect: "mysql",
+  define: {
+      timestamp: true,
+      underscored: true
+  }
+}
---
+++ src/server.js
@@ -0,0 +1,4 @@
+
+const PORT = 3333;
+
+console.log("Rondando na porta" + PORT);
